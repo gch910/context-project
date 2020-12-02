e6c4f481eb8c5d008454795dbcf0bfb3faa1a484
Dorn <dornkholland@gmail.com>
2020-12-01 17:20:27 -0700

    completed 02 and 03

--- problems/02-make-dog.js
+++ problems/02-make-dog.js
@@ -2,7 +2,7 @@
 Below we've declared a static function named `makeJet` for you that will return
 an instance of the Dog class.
 
-You will be writing two new methods on the class below. 
+You will be writing two new methods on the class below.
 These two methods wil be: changeName(newName) and speak(word).
 The speak function will intake a word and then return a sentence with the name
 of the dog saying that word. The changeName function will intake a newName and
@@ -10,7 +10,7 @@ will set the dog object's name to be the passed in name. See below for examples:
 
 
 Examples:
-let dog1 = Dog.makeJet(); // returns an object 
+let dog1 = Dog.makeJet(); // returns an object
 
 console.log(dog1.name); // Jet
 console.log(dog1.speak("hello")); // Jet says hello
@@ -29,6 +29,12 @@ class Dog {
   }
   static makeJet() {
     return new this('Jet');
+  }
+  speak(word) {
+    return `${this.name} says ${word}`;
+  }
+  changeName(name) {
+    return this.name = name;
   }
 }
 
--- problems/03-change-context.js
+++ problems/03-change-context.js
@@ -1,7 +1,7 @@
 /***********************************************************************
-Write a function named `changeContext(function, object)` that will accept a
+Write a function named `changeContext(func, object)` that will accept a
 function and an object. The changeContext function should return the result of
-the function being invoked with the passed in object as it's context. 
+the function being invoked with the passed in object as it's context.
 
 Take careful note of how this is invoked:
 
@@ -21,8 +21,10 @@ console.log(changeContext(getSecret, bat)); // prints "I'm scared of the dark"
 console.log(changeContext(getSecret, map)); // prints "I don't know where I'm going"
 
 ***********************************************************************/
-
-// your code here!
+function changeContext(func, object) {
+  let bindedFunction = func.bind(object);
+  return bindedFunction();
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
