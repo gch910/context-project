8801168b85b47f8c1fe4f449c38f4a099a04f1f1
Geb <gch910@gmail.com>
2020-12-01 18:22:42 -0600

    before pull

--- problems/02-make-dog.js
+++ problems/02-make-dog.js
@@ -2,7 +2,7 @@
 Below we've declared a static function named `makeJet` for you that will return
 an instance of the Dog class.
 
-You will be writing two new methods on the class below. 
+You will be writing two new methods on the class below.
 These two methods wil be: changeName(newName) and speak(word).
 The speak function will intake a word and then return a sentence with the name
 of the dog saying that word. The changeName function will intake a newName and
@@ -10,12 +10,12 @@ will set the dog object's name to be the passed in name. See below for examples:
 
 
 Examples:
-let dog1 = Dog.makeJet(); // returns an object 
+let dog1 = Dog.makeJet(); // returns an object
 
 console.log(dog1.name); // Jet
 console.log(dog1.speak("hello")); // Jet says hello
 console.log(dog1.changeName("Freyja")); // Freyja
-console.log(dog1.name); // Freyja
+console.log(dog1.name); // Freyja 
 console.log(dog1.speak("hello")); // Freyja says hello
 
 let dog2 = Dog.makeJet();
